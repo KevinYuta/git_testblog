82f1e7b2d35407539a3954c6a45672892c304e27
EC2 Default User <ec2-user@ip-172-31-31-43.ap-southeast-1.compute.internal>
2019-05-14 10:01:15 +0000

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root 'text#index'
-  resourses :text,only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :text,only: [:new, :create, :show, :edit, :update, :destroy]
 end
