f59985f49add21d418b0c52a14b35b2f6b6c976f
EC2 Default User <ec2-user@ip-172-31-31-43.ap-southeast-1.compute.internal>
2019-05-14 10:14:36 +0000

    textコントローラーのcreateアクションとストロングパラメーターの定義

--- app/controllers/text_controller.rb
+++ app/controllers/text_controller.rb
@@ -5,4 +5,13 @@ class TextController < ApplicationController
   
   def new
   end
+  
+  def create
+    Text.create(text_params)
+  end
+  
+  private
+  def text_params
+    params.permit(:text)
+  end
 end
